39c6cc3d0a156f690045a542d2ed822660f4f245
Emanuel Ramírez <er6348100@gmail.com>
2026-03-20 17:32:05 -0600

    Fin ej 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine();
+﻿using System.Threading.Channels;
+
+int edad;
+bool correcto;
+
+do
+{
+    Console.WriteLine("Ingrese su edad");
+    Console.WriteLine();
+    correcto = int.TryParse(Console.ReadLine(), out edad);
+    Console.WriteLine();
+    if (!correcto)
+    {
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+        
+    }
+    else if(edad<0 || edad>100)
+    {
+        Console.WriteLine("Ingrese una edad válida");
+        Console.WriteLine();
+        
+    }
+    Console.Clear();
+} while (!correcto || edad<0 || edad>100);
+Console.WriteLine("Gracias por ingresar una edad válida");
