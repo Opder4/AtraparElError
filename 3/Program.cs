f6c2317331aded929033f76922bc0dd68e42019e
Emanuel Ramírez <er6348100@gmail.com>
2026-03-20 17:39:57 -0600

    Fin ej 3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int cantidadEs;
+do
+{
+    Console.WriteLine("Ingrese la cantidad de estudiantes");
+    Console.WriteLine();
+    int.TryParse(Console.ReadLine(), out cantidadEs);
+    Console.WriteLine();
+    Console.Clear();
+}while (cantidadEs<=0);
+Console.WriteLine("Cantidad Válida");
