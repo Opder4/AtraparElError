12cc941e9b61b1ab5d9bf38bb119652f8c625d48
Emanuel Ramírez <er6348100@gmail.com>
2026-03-20 17:36:55 -0600

    Fin ej 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿double precio;
+
+do
+{
+    Console.WriteLine("Ingrese el precio del producto");
+    Console.WriteLine();
+    double.TryParse(Console.ReadLine(), out precio);
+    Console.ReadLine();
+    Console.Clear();
+} while (precio <= 0);
+Console.WriteLine("Precio Válido");
