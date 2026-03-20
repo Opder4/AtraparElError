5d4164f2d94db402a3000a2004d4b42b55e893bb
Emanuel Ramírez <er6348100@gmail.com>
2026-03-20 17:59:43 -0600

    Fin ej 6

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿DateOnly fecha;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese la fecha (yyyy-mm-dd)");
+    Console.WriteLine();
+    correcto = DateOnly.TryParse(Console.ReadLine(), out fecha);
+    Console.WriteLine();
+    if (!correcto)
+    {
+        Console.WriteLine("Fecha no válida");
+        Console.WriteLine();
+        Console.ReadLine();
+        Console.Clear();
+    }
+} while (!correcto);
+Console.WriteLine(fecha);
