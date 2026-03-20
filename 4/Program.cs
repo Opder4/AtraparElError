acee3ac7259702824b125067c6b06336249ab5cb
Emanuel Ramírez <er6348100@gmail.com>
2026-03-20 17:48:26 -0600

    Fin ej 4

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int nacimiento=0;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese su año de nacimiento");
+    Console.WriteLine();
+    correcto=int.TryParse(Console.ReadLine(), out nacimiento);
+    Console.WriteLine();
+    if (!correcto)
+    {
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+    }
+    else if (nacimiento <1950 || nacimiento>2026)
+    {
+        Console.WriteLine("Año no válido");
+        Console.WriteLine();
+    }
+    Console.ReadLine();
+    Console.Clear();
+}while (!correcto || nacimiento<1950 || nacimiento>2026);
+Console.WriteLine("Año válido");
