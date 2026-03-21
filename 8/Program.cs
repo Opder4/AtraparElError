f51e07fe0fa8f10646519c0baad68fa3897fc9d4
Emanuel Ramírez <er6348100@gmail.com>
2026-03-20 18:32:04 -0600

    Fin ej 8

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System.Data.SqlTypes;
+
+double numero;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese un número");
+    Console.WriteLine();
+    correcto = double.TryParse(Console.ReadLine(), out numero);
+    Console.WriteLine();
+    if (!correcto)
+    {
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+        Console.ReadLine();
+        Console.Clear();
+    }
+    else if (numero<0)
+    {
+        Console.WriteLine("Número no válido");
+        Console.WriteLine();
+        Console.ReadLine();
+        Console.Clear();
+    }
+}while (!correcto || numero<0);
+Console.WriteLine($"La raíz cuadrada de {numero} es {Math.Sqrt(numero)}");
