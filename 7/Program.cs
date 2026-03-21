5ea0c6d913c3f9a08b22954f22ad43c3850a4382
Emanuel Ramírez <er6348100@gmail.com>
2026-03-20 18:03:29 -0600

    Fin ej 7

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿double salario;
+bool correcto;
+do
+{
+    Console.WriteLine("Ingrese su salario");
+    Console.WriteLine();
+    correcto = double.TryParse(Console.ReadLine(), out salario);
+    Console.WriteLine();
+    if (!correcto)
+    {
+        Console.WriteLine("Dato no válido");
+        Console.WriteLine();
+        Console.ReadLine();
+        Console.Clear();
+    }
+    else if (salario<0)
+    {
+        Console.WriteLine("Salario no válido");
+        Console.WriteLine();
+        Console.ReadLine();
+        Console.Clear();
+    }
+} while (!correcto || salario<0);
+Console.WriteLine("Salario válido");
